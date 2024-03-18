d798a899dda45a9f3ae0f787cf3ba0348e227afb
kaydjay <khadijatrasaq@gmail.com>
2024-03-18 17:26:28 +0100

    Tasks amended

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-""" The BAsics of async """
+"""
+    The function `wait_random` is an asynchronous coroutine that takes an integer
+    argument and returns a random float after a delay.
+    
+    :param max_delay: The `max_delay` parameter in the `wait_random` function is an
+    integer that specifies the maximum delay in seconds for the asynchronous
+    coroutine to wait before returning a random float value, defaults to 10
+    :type max_delay: int (optional)
+    :return: The `wait_random` coroutine returns a float value representing the
+    delay that was randomly generated and waited for before returning.
+    """
 
 import asyncio
 import random
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-""" To execute multiple coroutines at the same time """
+"""
+    The `wait_n` function executes multiple coroutines simultaneously and returns a
+    sorted list of the delays.
+    
+    :param n: The parameter `n` in the `wait_n` function represents the number of
+    coroutines you want to execute simultaneously. It determines how many coroutines
+    will be created and executed concurrently using asyncio
+    :type n: int
+    :param max_delay: The `max_delay` parameter in the `wait_n` function represents
+    the maximum delay in seconds that each coroutine should wait before completing.
+    This value is used when calling the `wait_random` coroutine to generate a random
+    delay between 0 and `max_delay` seconds
+    :type max_delay: int
+    :return: The `wait_n` function returns a sorted list of floats representing the
+    delays returned by executing multiple coroutines concurrently using asyncio.
+    """
+
 
 import asyncio
 from typing import List
