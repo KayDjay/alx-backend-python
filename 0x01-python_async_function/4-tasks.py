a5c43b29e01e62de669f1af92a91a0e36fd5eaae
kaydjay <khadijatrasaq@gmail.com>
2024-03-19 05:42:44 +0100

    Task amends

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,18 +6,18 @@ from typing import List
 
 task_wait_random = __import__("3-tasks"
                               ).task_wait_random
-"""
-    Executes multiple instances of the task_wait_random coroutine concurrently.
-
-    Args:
-        n (int): The number of tasks to execute.
-        max_delay (int): The maximum delay for each task.
-
-    Returns:
-        List[float]: A list of floats representing the delays of each task.
-"""
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+        Executes multiple instances of the task_wait_random coroutine concurrently.
+
+        Args:
+            n (int): The number of tasks to execute.
+            max_delay (int): The maximum delay for each task.
+
+        Returns:
+            List[float]: A list of floats representing the delays of each task.
+    """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     return await asyncio.gather(*tasks)
